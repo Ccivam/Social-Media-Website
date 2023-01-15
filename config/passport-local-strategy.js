dbab67a329cd87b82e9960e8a7ed4d20583c9192
Shivam Kumar <shivamkumar4062390@gmail.com>
2023-01-16 00:31:56 +0530

    added flashed

--- config/passport-local-strategy.js
+++ config/passport-local-strategy.js
@@ -4,14 +4,17 @@ const User=require('../model/user');
 
 //Authentication using passport
 passport.use(new LocalStrategy({
-   usernameField:'email'
+   usernameField:'email',
+   passReqToCallback:true
 },
-function(email,password,done){
+function(req,email,password,done){
     //find a user and established the identity
    User.findOne({email:email},function(err,user){
-       if(err){console.log('error in finding user');return done(err);}
+       if(err){
+        req.flash('error',err);
+       }
        if(!user||user.password!=password){
-        console.log('Invalid username password');
+        req.flash('error','Invalid username/password');
         return done(null,false);
        }
       return done(null,user);
